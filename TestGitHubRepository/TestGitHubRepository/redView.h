24071e098789d609b780abf61232f7521433a41c
孟凡迪 <564313937@qq.com>
2017-06-12 18:11:05 +0800

    03-创建了一个类

---
+++ TestGitHubRepository/TestGitHubRepository/redView.h
@@ -0,0 +1,13 @@
+//
+//  redView.h
+//  TestGitHubRepository
+//
+//  Created by 孟凡迪 on 2017/6/12.
+//  Copyright © 2017年 孟凡迪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface redView : UIView
+
+@end
---
+++ TestGitHubRepository/TestGitHubRepository/redView.m
@@ -0,0 +1,21 @@
+//
+//  redView.m
+//  TestGitHubRepository
+//
+//  Created by 孟凡迪 on 2017/6/12.
+//  Copyright © 2017年 孟凡迪. All rights reserved.
+//
+
+#import "redView.h"
+
+@implementation redView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
